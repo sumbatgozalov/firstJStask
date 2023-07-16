510c729523118da132497008d3afa7ae26624344
Sumbat Gozalov <gozalovsumbat@yahoo.com>
2023-07-16 11:26:00 +0400

    end of first task

--- first.js
+++ first.js
@@ -1,1 +1,68 @@
+//Task 1
+// let nameofTask = prompt("Enter name")
+// document.write(nameofTask)
+
+//Task
+// let a = prompt("Enter first number")
+// let b = prompt("Enter second number")
+
+// let sum = parseInt(a)+parseInt(b)
+// console.log(sum)
+// alert(sum)
+
+//Task3
+// let number = prompt("Enter number greater than 100")
+
+// while(1){
+//     if(number>100){
+//         alert("Number is acceptableI" + number);
+//         break;
+//     }else{
+//         number = prompt("Enter number that match the condition ")
+//     }
+
+// }
+
+// Task 4
+// let number = prompt("Enter number not anything else")
+// if (number>0){
+//     alert('Number is greater than 0')
+// }
+// else if (number<0){
+//     alert('Number is lower than 0')
+// }
+//  else if (number==0){
+//     alert('Number is equal to 0')
+// }
+// else{
+//     alert('I said enter number not anything else!!!')}
+
+// Task 5
+
+// let message;
+
+// let login = prompt();
+
+// if (login == "Employee") {
+//   alert((message = "Hello"));
+// } else if (login == "Director") {
+//   alert((message = "Salam Müəllim"));
+// } else if (login == "") {
+//   alert((message = "No login"));
+// } else {
+//   alert((message = "Who the hell are you?"));
+// }
+
+// Task 6 
+
+
+
+// let a = prompt("first")
+// let b = prompt("second")
+// let sum;
+// function pow() {
+//     sum = a**b;
+//     return sum   
+// }
+// alert(pow())
 
